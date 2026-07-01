91cdca66f301b3aedbea4d4b7118ebf3111db356
vietanh9005 <vietanh9018@gmail.com>
2026-07-01 18:19:24 +0700

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,19 +5,36 @@ import dns from "dns";
 import dotenv from "dotenv";
 import { fileURLToPath } from "node:url";
 import cors from "cors";
+import path from "path";
 
 dotenv.config({ path: fileURLToPath(new URL("./.env", import.meta.url)) });
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const PORT = process.env.PORT || 8008;
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 
 app.use("/api/tasks", tasksRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:8008/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:8008/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
